4f154af862eab3135aecb7b0e635c7319ffa791c
Marc Dingena <marc.dingena@gmail.com>
2021-07-23 13:21:43 +0800

    Improve exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-export type { DecodedString } from './decodeString';
-export type { Prefab } from './decoders';
+/**
+ * COMPONENTS
+ */
+import * as components from './components/transcoders';
+export type BasicDecay = components.BasicDecay.Component;
+export type DurabilityModule = components.DurabilityModule.Component;
+export type HeatSourceBase = components.HeatSourceBase.Component;
+export type LiquidContainer = components.LiquidContainer.Component;
+export type NetworkRigidbody = components.NetworkRigidbody.Component;
+export type PhysicalMaterialPart = components.PhysicalMaterialPart.Component;
+export type Pickup = components.Pickup.Component;
+export type PickupDock = components.PickupDock.Component;
+export type PopulationSpawnArea = components.PopulationSpawnArea.Component;
+export type SpawnArea = components.SpawnArea.Component;
+export type StatManager = components.StatManager.Component;
+export type WoodcutTree = components.WoodcutTree.Component;
 
+/**
+ * EMBEDDED ENTITIES
+ */
+export * from './embeddedEntities';
+
+/**
+ * OBJECT SHAPES
+ */
+export type { DecodedString } from './decodeString';
+export type {
+  Prefab,
+  PrefabObject,
+  Components,
+  EmbeddedEntities,
+  KnownEmbeddedEntity,
+  UnknownEmbeddedEntity,
+  ChildPrefab
+} from './decoders';
+export type { Component, KnownComponent, UnknownComponent } from './components';
+
+/**
+ * HASHES
+ */
 export { ComponentHash } from './ComponentHash';
-export { createString } from './createString';
-export { decodeString } from './decodeString';
 export { EmbeddedEntityHash } from './EmbeddedEntityHash';
 export { PhysicalMaterialPartHash } from './PhysicalMaterialPartHash';
 export { PopulationDefinitionHash } from './PopulationDefinitionHash';
@@ -11,4 +46,14 @@ export { PrefabEmbeddedEntityHash } from './PrefabEmbeddedEntityHash';
 export { PrefabHash } from './PrefabHash';
 export { PresetHash } from './PresetHash';
 export { SpeciesHash } from './SpeciesHash';
+
+/**
+ * UTILITIES
+ */
 export * from './utils';
+
+/**
+ * ATT STRING TRANSCODER
+ */
+export { createString } from './createString';
+export { decodeString } from './decodeString';
