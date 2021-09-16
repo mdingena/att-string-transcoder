72ab64a587ef83cafe7fb0c03f6ec46cd71fa4e7
Marc Dingena <marc.dingena@gmail.com>
2021-09-16 20:17:22 +0800

    Update tests

--- tests/decodeString.test.ts
+++ tests/decodeString.test.ts
@@ -3,7 +3,7 @@ import { encodedEmptyFlask } from './data/encodedStrings';
 import { decodedEmptyFlask } from './data/decodedStrings';
 
 describe('decodeString', () => {
-  it('should decode a string of UInts', () => {
+  it('decodes a string of UInts', () => {
     const decodedString = decodeString(encodedEmptyFlask);
 
     expect(decodedString).toStrictEqual(decodedEmptyFlask);
