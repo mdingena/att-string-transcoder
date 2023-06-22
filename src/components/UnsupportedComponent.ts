4e58346ee2567df5c2f37e463bdcf9ba85be0045
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 14:35:25 +0800

    Create UnsupportedComponent.ts

---
+++ src/components/UnsupportedComponent.ts
@@ -0,0 +1,42 @@
+import type { BinaryReader } from '../BinaryReader.js';
+import type { BinaryString } from '../types/BinaryString.js';
+import { Component, type ComponentProps } from './Component.js';
+
+export type UnsupportedComponentProps = {
+  hash: number;
+  name: string;
+  rawData: BinaryString;
+};
+
+export class UnsupportedComponent extends Component {
+  rawData: BinaryString;
+
+  constructor({ hash, name, rawData, version }: ComponentProps & UnsupportedComponentProps) {
+    super({ hash, name, version });
+
+    this.rawData = rawData;
+  }
+
+  static override fromBinary(
+    reader: BinaryReader,
+    version: number,
+    hash: number,
+    name: string,
+    dataLength: number
+  ): UnsupportedComponent {
+    return new UnsupportedComponent({
+      hash,
+      name,
+      version,
+      rawData: reader.readBits(dataLength)
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  override toBinary(_ = this.version): BinaryString {
+    /**
+     * @property {BinaryString} rawData
+     */
+    return this.rawData;
+  }
+}
