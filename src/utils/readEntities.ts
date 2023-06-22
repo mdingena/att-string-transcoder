9594a85baa2e67810f0447d16e281a05cfda77d9
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 21:09:48 +0800

    Make componentVersions argument optional

--- src/utils/readEntities.ts
+++ src/utils/readEntities.ts
@@ -1,14 +1,15 @@
 import type { BinaryReader } from '../BinaryReader.js';
 import type { PrefabEntities } from '../types/PrefabEntities.js';
 import type { UnknownPrefabEntities } from '../types/UnknownPrefabEntities.js';
-import { terminatorHash } from '../constants.js';
+import { latestSupportedComponentVersions, terminatorHash } from '../constants.js';
 import { Entity } from '../entities/Entity.js';
 import { EntityHash } from '../types/EntityHash.js';
 
 type KnownPrefabEntities = Record<string, Entity>;
 
-export function readEntities(reader: BinaryReader, componentVersions: Map<number, number>): PrefabEntities {
+export function readEntities(reader: BinaryReader, componentVersions?: Map<number, number>): PrefabEntities {
   const entities = Object.assign({} as KnownPrefabEntities, { Unknown: [] } as UnknownPrefabEntities);
+  const versions = componentVersions ?? latestSupportedComponentVersions;
 
   const isLooping = true;
 
@@ -22,7 +23,7 @@ export function readEntities(reader: BinaryReader, componentVersions: Map<number
     /* Read and ignore the entity's data length. */
     reader.readUnsignedInteger();
 
-    const entity = Entity.fromBinary(reader, componentVersions, { hash, name });
+    const entity = Entity.fromBinary(reader, versions, { hash, name });
 
     if (entity.name === 'Unknown') {
       entities.Unknown.push(entity);
