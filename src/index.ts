660c40ae2afd0e9451cc3143b7743b5eeee656ce
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 15:34:44 +0800

    Add library exports

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+/* Types */
+export type { AngularVelocity } from './types/AngularVelocity.js';
+export type { ATTEntityName } from './types/ATTEntityName.js';
+export type { ATTPrefabHash } from './types/ATTPrefabHash.js';
+export type { ATTPrefabName } from './types/ATTPrefabName.js';
+export type { BinaryString } from './types/BinaryString.js';
+export type { Position } from './types/Position.js';
+export type { PrefabChild } from './types/PrefabChild.js';
+export type { PrefabComponents } from './types/PrefabComponents.js';
+export type { PrefabEntities } from './types/PrefabEntities.js';
+export type { Rotation } from './types/Rotation.js';
+export type { SaveString } from './types/SaveString.js';
+export type { SupportedComponent } from './types/SupportedComponent.js';
+export type { SupportedPrefabComponents } from './types/SupportedPrefabComponents.js';
+export type { SupportedPrefabEntities } from './types/SupportedPrefabEntities.js';
+export type { UnknownPrefabComponents } from './types/UnknownPrefabComponents.js';
+export type { UnknownPrefabEntities } from './types/UnknownPrefabEntities.js';
+export type { UnsupportedPrefabComponents } from './types/UnsupportedPrefabComponents.js';
+export type { UnsupportedPrefabEntities } from './types/UnsupportedPrefabEntities.js';
+export type { Velocity } from './types/Velocity.js';
+
+/* Enums and enum-likes */
+export { ATTPrefabs } from './types/ATTPrefabs.js';
+export { ComponentHash } from './types/ComponentHash.js';
+export { EntityHash } from './types/EntityHash.js';
+export { PhysicalMaterialPartHash } from './types/PhysicalMaterialPartHash.js';
+export { PopulationDefinitionHash } from './types/PopulationDefinitionHash.js';
+export { PresetHash } from './types/PresetHash.js';
+export { SpeciesHash } from './types/SpeciesHash.js';
+
+/* Prefabs */
+export { Prefab } from './Prefab.js';
+
+/* Binary classes */
+export { BinaryData } from './BinaryData.js';
+export { BinaryReader } from './BinaryReader.js';
+export { BinaryWriter } from './BinaryWriter.js';
+
+/* Components */
+export { BasicDecayComponent } from './components/BasicDecayComponent.js';
+export { Component } from './components/Component.js';
+export { DurabilityModuleComponent } from './components/DurabilityModuleComponent.js';
+export { FireComponent } from './components/FireComponent.js';
+export { FuseComponent } from './components/FuseComponent.js';
+export { HeatSourceBaseComponent } from './components/HeatSourceBaseComponent.js';
+export { LiquidContainerComponent } from './components/LiquidContainerComponent.js';
+export { NetworkRigidbodyComponent } from './components/NetworkRigidbodyComponent.js';
+export { PhysicalMaterialPartComponent } from './components/PhysicalMaterialPartComponent.js';
+export { PickupComponent } from './components/PickupComponent.js';
+export { PickupDockComponent } from './components/PickupDockComponent.js';
+export { PopulationSpawnAreaComponent } from './components/PopulationSpawnAreaComponent.js';
+export { SentGiftComponent } from './components/SentGiftComponent.js';
+export { SpawnAreaComponent } from './components/SpawnAreaComponent.js';
+export { StatManagerComponent } from './components/StatManagerComponent.js';
+export { UnsupportedComponent } from './components/UnsupportedComponent.js';
+export { WoodcutTreeComponent } from './components/WoodcutTreeComponent.js';
+
+/* Entities */
+export { Entity } from './entities/Entity.js';
+export { FireEntity } from './entities/FireEntity.js';
+
+/* Utilities */
+export { isATTEntityName } from './utils/isATTEntityName.js';
+export { isATTPrefabHash } from './utils/isATTPrefabHash.js';
+export { isATTPrefabName } from './utils/isATTPrefabName.js';
+export { isEmbeddableEntity } from './utils/isEmbeddableEntity.js';
+export { isSavableComponent } from './utils/isSavableComponent.js';
+export { isUnknownComponent } from './utils/isUnknownComponent.js';
+export { readChildren } from './utils/readChildren.js';
+export { readComponent } from './utils/readComponent.js';
+export { readComponents } from './utils/readComponents.js';
+export { readEntities } from './utils/readEntities.js';
+export { writeChildren } from './utils/writeChildren.js';
+export { writeComponents } from './utils/writeComponents.js';
+export { writeEntities } from './utils/writeEntities.js';
