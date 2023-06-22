ed86795123a64b2d172561681a3b8b43d7a22f73
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 14:37:26 +0800

    Create FuseComponent.ts

---
+++ src/components/FuseComponent.ts
@@ -0,0 +1,90 @@
+import type { BinaryReader } from '../BinaryReader.js';
+import type { BinaryString } from '../types/BinaryString.js';
+import { BinaryWriter } from '../BinaryWriter.js';
+import { Component, type ComponentProps } from './Component.js';
+import { ComponentHash } from '../types/ComponentHash.js';
+
+type FuseComponentPropsV1 = {
+  isFinished?: boolean;
+  isLit?: boolean;
+  currentFuseAmount?: number;
+};
+
+type FuseComponentProps = FuseComponentPropsV1;
+
+export class FuseComponent extends Component {
+  isFinished: boolean;
+  isLit: boolean;
+  currentFuseAmount: number;
+
+  constructor({ version, isFinished, isLit, currentFuseAmount }: ComponentProps & FuseComponentProps) {
+    const hash = ComponentHash.Fuse;
+    const name = 'Fuse';
+
+    super({ hash, name, version });
+
+    this.isFinished = isFinished ?? false;
+    this.isLit = isLit ?? false;
+    this.currentFuseAmount = currentFuseAmount ?? 1;
+  }
+
+  static override fromBinary(reader: BinaryReader, version: number): FuseComponent {
+    const props: ComponentProps & FuseComponentProps = { version };
+
+    /**
+     * @property {boolean} isFinished
+     * @since v1
+     */
+    if (props.version >= 1) {
+      props.isFinished = reader.readBoolean();
+    }
+
+    /**
+     * @property {boolean} isLit
+     * @since v1
+     */
+    if (props.version >= 1) {
+      props.isLit = reader.readBoolean();
+    }
+
+    /**
+     * @property {float} currentFuseAmount
+     * @since v1
+     */
+    if (props.version >= 1) {
+      props.currentFuseAmount = reader.readFloat();
+    }
+
+    return new FuseComponent(props);
+  }
+
+  override toBinary(version = this.version): BinaryString {
+    const writer = new BinaryWriter();
+
+    /**
+     * @property {boolean} isFinished
+     * @since v1
+     */
+    if (version >= 1) {
+      writer.writeBoolean(this.isFinished);
+    }
+
+    /**
+     * @property {boolean} isLit
+     * @since v1
+     */
+    if (version >= 1) {
+      writer.writeBoolean(this.isLit);
+    }
+
+    /**
+     * @property {float} currentFuseAmount
+     * @since v1
+     */
+    if (version >= 1) {
+      writer.writeFloat(this.currentFuseAmount);
+    }
+
+    return writer.flush();
+  }
+}
