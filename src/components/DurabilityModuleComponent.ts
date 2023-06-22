b1471477ff8b2b24e95d6ef48c4736a6c319914b
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 14:36:11 +0800

    Create DurabilityModuleComponent.ts

---
+++ src/components/DurabilityModuleComponent.ts
@@ -0,0 +1,52 @@
+import type { BinaryReader } from '../BinaryReader.js';
+import type { BinaryString } from '../types/BinaryString.js';
+import { BinaryWriter } from '../BinaryWriter.js';
+import { Component, type ComponentProps } from './Component.js';
+import { ComponentHash } from '../types/ComponentHash.js';
+
+type DurabilityModuleComponentPropsV1 = {
+  integrity?: number;
+};
+
+type DurabilityModuleComponentProps = DurabilityModuleComponentPropsV1;
+
+export class DurabilityModuleComponent extends Component {
+  integrity: number;
+
+  constructor({ version, integrity }: ComponentProps & DurabilityModuleComponentProps) {
+    const hash = ComponentHash.DurabilityModule;
+    const name = 'DurabilityModule';
+
+    super({ hash, name, version });
+
+    this.integrity = integrity ?? 1;
+  }
+
+  static override fromBinary(reader: BinaryReader, version: number): DurabilityModuleComponent {
+    const props: ComponentProps & DurabilityModuleComponentProps = { version };
+
+    /**
+     * @property {float} integrity
+     * @since v1
+     */
+    if (props.version >= 1) {
+      props.integrity = reader.readFloat();
+    }
+
+    return new DurabilityModuleComponent(props);
+  }
+
+  override toBinary(version = this.version): BinaryString {
+    const writer = new BinaryWriter();
+
+    /**
+     * @property {float} integrity
+     * @since v1
+     */
+    if (version >= 1) {
+      writer.writeFloat(this.integrity);
+    }
+
+    return writer.flush();
+  }
+}
