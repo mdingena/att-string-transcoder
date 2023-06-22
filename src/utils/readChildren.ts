11e02fd97b992947f54d8d3a6e79134fa1631d10
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 14:59:44 +0800

    Create readChildren.ts

---
+++ src/utils/readChildren.ts
@@ -0,0 +1,22 @@
+import type { BinaryReader } from '../BinaryReader.js';
+import type { PrefabChild } from '../types/PrefabChild.js';
+import { Prefab } from '../Prefab.js';
+
+export function readChildren(reader: BinaryReader, componentVersions: Map<number, number>): PrefabChild[] {
+  const children: PrefabChild[] = [];
+
+  const isLooping = true;
+
+  while (isLooping) {
+    const hasChild = reader.readBoolean();
+
+    if (!hasChild) break;
+
+    const parentHash = reader.readUnsignedInteger();
+    const prefab = Prefab.fromBinary(reader, componentVersions);
+
+    children.push({ parentHash, prefab });
+  }
+
+  return children;
+}
