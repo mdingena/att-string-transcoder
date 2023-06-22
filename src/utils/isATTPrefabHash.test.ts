d56d24a59238bf0cd3039e8c5e161f834a637f97
Marc Dingena <marc.dingena@gmail.com>
2023-06-22 15:37:36 +0800

    Create isATTPrefabHash.test.ts

---
+++ src/utils/isATTPrefabHash.test.ts
@@ -0,0 +1,24 @@
+import { ATTPrefabs } from '../types/ATTPrefabs.js';
+import { isATTPrefabHash } from './isATTPrefabHash.js';
+
+describe('isATTPrefabHash()', () => {
+  describe('when given a valid prefab hash', () => {
+    it('returns true', () => {
+      const validHash = ATTPrefabs.Handle_Short.hash;
+
+      const result = isATTPrefabHash(validHash);
+
+      expect(result).toBe(true);
+    });
+  });
+
+  describe('when given an invalid prefab hash', () => {
+    it('returns true', () => {
+      const invalidHash = 0;
+
+      const result = isATTPrefabHash(invalidHash);
+
+      expect(result).toBe(false);
+    });
+  });
+});
