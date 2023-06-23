defa72af74d59047aef7bd0a8a3f977b314492ac
Marc Dingena <marc.dingena@gmail.com>
2023-06-23 16:23:53 +0800

    Create readEntity.ts

---
+++ src/utils/readEntity.ts
@@ -0,0 +1,24 @@
+import type { BinaryReader } from '../BinaryReader.js';
+import { supportedEntities } from '../constants.js';
+import { Entity } from '../entities/Entity.js';
+import { UnsupportedEntity } from '../entities/UnsupportedEntity.js';
+
+export function readEntity(
+  reader: BinaryReader,
+  hash: number,
+  name: string,
+  componentVersions?: Map<number, number>
+): Entity {
+  /* Get the entity's data length and ignore it. */
+  reader.readUnsignedInteger();
+
+  /* Get the entity's class. */
+  const SupportedEntity = supportedEntities[name];
+
+  /* Create an entity instance from the binary data string. */
+  if (typeof SupportedEntity === 'undefined') {
+    return UnsupportedEntity.fromBinary(reader, { hash, name, componentVersions });
+  } else {
+    return SupportedEntity.fromBinary(reader, componentVersions);
+  }
+}
