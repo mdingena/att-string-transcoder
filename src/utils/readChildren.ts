e2ae7ef267c85bc58831366eba1fdc7f76a36d11
Marc Dingena <marc.dingena@gmail.com>
2023-07-24 16:48:55 +0800

    Update readChildren.ts

--- src/utils/readChildren.ts
+++ src/utils/readChildren.ts
@@ -3,6 +3,14 @@ import type { PrefabChild } from '../types/PrefabChild.js';
 
 import { Prefab } from '../Prefab.js';
 
+/**
+ * Reads all child prefabs from the given `BinaryReader` and returns an array of `PrefabChild`.
+ *
+ * @example
+ * import { readChildren } from 'att-string-transcoder';
+ *
+ * const children = readChildren(reader);
+ */
 export function readChildren(reader: BinaryReader, componentVersions?: Map<number, number>): PrefabChild[] {
   const children: PrefabChild[] = [];
 
