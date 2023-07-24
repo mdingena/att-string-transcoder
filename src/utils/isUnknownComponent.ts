40477eb70d6c64483554badba2d62d6c2ea77ac7
Marc Dingena <marc.dingena@gmail.com>
2023-07-24 16:47:12 +0800

    Update isUnknownComponent.ts

--- src/utils/isUnknownComponent.ts
+++ src/utils/isUnknownComponent.ts
@@ -1,6 +1,16 @@
 import type { Component } from '../components/Component.js';
 import type { UnsupportedComponent } from '../components/UnsupportedComponent.js';
 
+/**
+ * Asserts if a `component` is an [`UnsupportedComponent`](https://github.com/mdingena/att-string-transcoder/tree/main/docs/UnsupportedComponent.md).
+ *
+ * @example
+ * import { isUnknownComponent } from 'att-string-transcoder';
+ *
+ * if (isUnknownComponent(component)) {
+ *   // `component` is of type `UnsupportedComponent`.
+ * }
+ */
 export function isUnknownComponent(component: Component): component is UnsupportedComponent {
   return component.name === 'Unknown';
 }
